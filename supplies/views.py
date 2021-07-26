07d97da8564c68580572975e95029136ec1d9c1b
JoeyyScott <joey-scott@live.com>
2021-07-26 20:13:54 +0000

    [ADD] Added search bar with associated view to supplies.html

--- supplies/views.py
+++ supplies/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Supply
 
 # Create your views here.
@@ -8,9 +10,20 @@ def all_supplies(request):
     """ A view to show all products, including sorting and search queries """
 
     supplies = Supply.objects.all()
+    query = None
+
+    if 'q' in request.GET:
+        query = request.GET['q']
+        if not query:
+            messages.error(request, "You didn't enter a search term!")
+            return redirect(reverse('supplies'))
+
+        queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(brand__icontains=query)
+        supplies = supplies.filter(queries)
 
     context = {
         'supplies': supplies,
+        'search_term': query,
     }
 
     return render(request, 'supplies/supplies.html', context)
