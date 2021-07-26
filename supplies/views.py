44a759081568cc2e6553db4683fd76dcff8c0906
JoeyyScott <joey-scott@live.com>
2021-07-26 15:37:57 +0000

    [ADD] Added supplies views and skeleton template

--- supplies/views.py
+++ supplies/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Supply
 
 # Create your views here.
+
+
+def all_supplies(request):
+    """ A view to show all products, including sorting and search queries """
+
+    supplies = Supply.objects.all()
+
+    context = {
+        'supplies': supplies,
+    }
+
+    return render(request, 'supplies/supplies.html', context)
