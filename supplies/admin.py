956a0406e00636de8e5cabfe6c348ebd7270d900
JoeyyScott <joey-scott@live.com>
2021-07-26 15:12:31 +0000

    [UPDATE] Updated admin in supplies app

--- supplies/admin.py
+++ supplies/admin.py
@@ -2,5 +2,22 @@ from django.contrib import admin
 from .models import Category, Supply
 
 
-admin.site.register(Category)
-admin.site.register(Supply)
+class SupplyAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'price',
+        'brand')
+    list_filter = ('category', 'brand')
+    search_fields = (
+        'name',
+        'brand',
+        'category__name',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Supply, SupplyAdmin)
