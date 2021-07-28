93121638c198cb93ccd99c2d7c9ad9353153e4ff
JoeyyScott <joey-scott@live.com>
2021-07-28 02:52:53 +0000

    [ADMIN] Added order to admin

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Order, CrateItems
 
-# Register your models here.
+
+class CrateItemsAdminInline(admin.TabularInline):
+    model = CrateItems
+    readonly_fields = ('crateitem_total',)
+    list_display = ('supply', )
+
+
+class OrderAdmin(admin.ModelAdmin):
+    inlines = (CrateItemsAdminInline,)
+    readonly_fields = ('order_number', 'date', 'order_total')
+
+    fields = ('date', 'order_number', 'full_name', 'email',
+              'contact_number', 'address_line_1', 'address_line_2',
+              'town_or_city', 'county', 'postcode', 'country', )
+
+    list_display = ('order_number', 'date', 'order_total',
+                    'full_name', 'email',)
+
+admin.site.register(Order, OrderAdmin)
