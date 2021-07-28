70c150f9aa5cc0c8cf4879f5adf2f2fd5b8af020
JoeyyScott <joey-scott@live.com>
2021-07-28 02:58:15 +0000

    [ADD] Added signal to update order total when supplies are added, edited or deleted from the crate

---
+++ checkout/signals.py
@@ -0,0 +1,19 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+
+from .models import CrateItems
+
+@receiver(post_save, sender=CrateItems)
+def update_on_save(sender, instance, created, **kwargs):
+    """
+    Update order total when items are added to or updated in the crate
+    """
+    instance.order.update_total()
+
+
+@receiver(post_save, sender=CrateItems)
+def update_on_delete(sender, instance, created, **kwargs):
+    """
+    Update order total when items are deleted from the crate
+    """
+    instance.order.update_total()
