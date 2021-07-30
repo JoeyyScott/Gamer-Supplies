28279b09ac153bdecf9fceeb6521becdef882b38
JoeyyScott <joey-scott@live.com>
2021-07-30 19:15:50 +0000

    [ADD] Added cache form data for checkout

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -33,13 +33,54 @@ form.addEventListener('submit', function(ev) {
     $('#submit-button').attr('disabled', true);
     $('#form-payment').fadeToggle(100);
     $('#loading-overlay').fadeToggle(100);
-    stripe.confirmCardPayment(clientSecret, { payment_method: { card: cardInput, }
-    }).then(function(result) {
-        if (result.error) {
-            var errorElement = document.getElementById('error-element');
-            var html = `<i class="fas fa-exclamation-triangle highlight"></i> <span class="altFont">${result.error.message}</span>`; $(errorElement).html(html);
-            $('#form-payment').fadeToggle(100); $('#loading-overlay').fadeToggle(100); cardInput.update({ 'disabled': false}); $('#submit-button').attr('disabled', false); 
-        } 
-        else { if (result.paymentIntent.status === 'succeeded') { form.submit(); } }
+    var saveInfo = Boolean($('#id-save-info').attr('checked'));
+    var csrfToken = $('input[name="csrfmiddlewaretoken"]').val();
+    var postData = {
+        'csrfmiddlewaretoken': csrfToken,
+        'client_secret': clientSecret,
+        'save_info': saveInfo,
+    };
+    var url = '/checkout/cache_checkout_data/';
+
+    $.post(url, postData).done(function () {
+        stripe.confirmCardPayment(clientSecret, {
+            payment_method: { 
+                card: cardInput,
+                billing_details: {
+                    name: $.trim(form.full_name.value),
+                    email: $.trim(form.email.value),
+                    phone: $.trim(form.contact_number.value),
+                    address: {
+                        line1: $.trim(form.address_line_1.value),
+                        line2: $.trim(form.address_line_2.value),
+                        city: $.trim(form.town_or_city.value),
+                        state: $.trim(form.county.value),
+                        country: $.trim(form.country.value),
+                    }
+                }
+            },
+            shipping: {
+                name: $.trim(form.full_name.value),
+                phone: $.trim(form.contact_number.value),
+                address: {
+                    line1: $.trim(form.address_line_1.value),
+                    line2: $.trim(form.address_line_2.value),
+                    city: $.trim(form.town_or_city.value),
+                    state: $.trim(form.county.value),
+                    postal_code: $.trim(form.postcode.value),
+                    country: $.trim(form.country.value),
+                }
+            },
+        }).then(function(result) {
+            if (result.error) {
+                var errorElement = document.getElementById('error-element');
+                var html = `<i class="fas fa-exclamation-triangle highlight"></i> <span class="altFont">${result.error.message}</span>`; $(errorElement).html(html);
+                $('#form-payment').fadeToggle(100); $('#loading-overlay').fadeToggle(100); cardInput.update({ 'disabled': false}); $('#submit-button').attr('disabled', false); 
+            } 
+            else { if (result.paymentIntent.status === 'succeeded') { form.submit(); } }
+        });
+    }).fail(function () {
+        // Reload the page, django messages will handle the error
+        location.reload();
     });
 });
