347857141eee6d2391b478d2350f7d9396ac36c8
JoeyyScott <joey-scott@live.com>
2021-07-30 01:42:30 +0000

    [ADD] Added basis for stripe webhook events

---
+++ checkout/webhook_handler.py
@@ -0,0 +1,32 @@
+from django.http import HttpResponse
+
+
+class Stripe_Controller:
+    """Functions for stripe webhooks"""
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """
+        Base handler for webhook events
+        """
+        return HttpResponse(
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handler for the stripe payment_intent.succeeded event
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handler for the stripe payment_intent.payment_failed event
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
