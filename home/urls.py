e5278c66e4ffbcea73c12436da100d2d8497a224
JoeyyScott <joey-scott@live.com>
2021-08-04 00:02:15 +0000

    [ADD] Added basic structure for review add feature

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('review_add/', views.review_add, name='review_add'),
 ]
--- home/views.py
+++ home/views.py
@@ -7,3 +7,9 @@ def index(request):
     """ Index page view """
 
     return render(request, 'home/index.html')
+
+
+def review_add(request):
+    """ Added review page view """
+
+    return render(request, 'home/review_add.html')
