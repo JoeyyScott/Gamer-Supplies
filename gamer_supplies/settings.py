2b0a511473da40441a6209beeb8205e00677a234
JoeyyScott <joey-scott@live.com>
2021-08-07 09:58:28 +0000

    [UPDATE] Updated secret_key in settings.py

--- gamer_supplies/settings.py
+++ gamer_supplies/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1t!^4%3b@_(qok8myrr8_egb9391x5gue!$bl@+agq4+qymwa7'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
