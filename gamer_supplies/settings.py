f642819dd727722267db4e81a3ac6d316ff84e16
JoeyyScott <joey-scott@live.com>
2021-08-07 11:01:08 +0000

    [ADD] Added email functionality to settings.py

--- gamer_supplies/settings.py
+++ gamer_supplies/settings.py
@@ -207,7 +207,18 @@ STRIPE_PUBLIC_KEY_GS = os.getenv('STRIPE_PUBLIC_KEY_GS', '')
 STRIPE_SECRET_KEY_GS = os.getenv('STRIPE_SECRET_KEY_GS', '')
 STRIPE_WH_SECRET_GS = os.getenv('STRIPE_WH_SECRET_GS', '')
 
-DEFAULT_FROM_EMAIL = 'gamersupplies@gmail.com'
+
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'gamersupplies@gmail.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 
 # Default primary key field type
