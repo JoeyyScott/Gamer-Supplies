b8fa4ef618808d69ef88d0884a3cccba71978f4c
JoeyyScott <joey-scott@live.com>
2021-08-07 07:05:22 +0000

    [TEST] Reconfigured wsgi application for deployment testing

--- gamer_supplies/wsgi.py
+++ gamer_supplies/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gamer_supplies.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gamer-supplies.settings')
 
 application = get_wsgi_application()
